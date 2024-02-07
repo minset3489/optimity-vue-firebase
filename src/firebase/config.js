65ab948bb9e72c61cf3e969a699dedb3fda78c87
Min Set <145420460+minset3489@users.noreply.github.com>
2024-02-07 22:02:26 +0630

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkDPOObEK-SRM4s81EfSVO_0gavCfqscc",
-  authDomain: "optimityweb-74380.firebaseapp.com",
-  projectId: "optimityweb-74380",
-  storageBucket: "optimityweb-74380.appspot.com",
-  messagingSenderId: "557617985895",
-  appId: "1:557617985895:web:e8ac3b8217088e006497c2"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
   // init firebase
